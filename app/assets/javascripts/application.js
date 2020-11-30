dec59c8a297899c32b165f8c837088bf37a34786
jia <422190416@qq.com>
2020-11-30 11:12:54 +0800

    user can login/logout/signup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,17 @@
 // about supported directives.
 //
 //= require rails-ujs
+
 //= require activestorage
+
 //= require turbolinks
+
 //= require bootstrap/alert
+
+//= require jquery
+
+//= require bootstrap-sprockets
+
+//= require bootstrap/dropdown
+
 //= require_tree .
